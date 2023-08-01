0d86b2e032f23ef771f07d19b695481c1d9683b2
Igor Maurell <igormaurell@gmail.com>
2023-08-01 13:12:50 -0300

    removing pycaches

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -285,7 +285,7 @@ class PrimitivesEmbeddingDGCNGn(nn.Module):
         if postprocess:
             np.random.seed(1234)
         np.random.shuffle(l)
-        random_index = torch.from_numpy(l) #removing 7000k points limitation
+        random_index = torch.from_numpy(l[:7000]) #removing 7000k points limitation
 
         if self.mode == 5:
             points = torch.cat([points, normals], dim=-1)
